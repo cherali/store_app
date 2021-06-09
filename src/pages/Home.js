8b9f5fd48b33b450a5f744c2213a88bc02089ff8
alich6 <alicheraghi148@gmail.com>
2021-06-09 21:05:07 +0430

    redirect --temporary

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
+import { useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
 
+// TODO: remove redirection
 function Home() {
+  const history = useHistory()
+
+  useEffect(() => {
+    history.replace('/product')
+  }, [history])
+
   return (
     <div>
       home
